cb73cb767df3d15bc642f2e1d9ecbfd2d79bd6ea
mattgellert <mattgellert@gmail.com>
2017-11-17 09:39:46 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { ConnectedRestaurantInput } from './components/RestaurantInput';
 import { ConnectedRestaurants } from './components/Restaurants';
 
+
 export class App extends Component {
   render() {
     return (
